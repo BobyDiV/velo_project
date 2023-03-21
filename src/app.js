0070898f45f629cdbb45f7acf1384db2274e94e9
Yuriy <kim.u.907@gmail.com>
2023-03-21 14:25:03 +0600

    release dev initial

---
+++ src/app.js
@@ -0,0 +1,56 @@
+require('dotenv').config();
+
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const session = require('express-session');
+const FileStore = require('session-file-store')(session);
+
+const ssrEngine = require('./middleware/ssrEngine');
+// const authCheck = require('./middleware/authCheck');
+
+const indexRouter = require('./routes/index.router');
+
+const app = express();
+
+app.use(morgan('dev'));
+// Чтобы наши статические файлы были видны браузеру, мы должны их подключить
+app.use(express.static(path.join(__dirname, '../public/')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Выносим порт в .env и на всякий случай подставляем дефолтный через ||
+
+const PORT = process.env.PORT ?? 3000;
+
+const sessionConfig = {
+  name: 'biscuitEater', // * Название куки
+  store: new FileStore(), // * подключение стора (БД для куки) для хранения
+  secret: process.env.SESSION_SECRET, // * ключ для шифрования куки
+  resave: false, // * если true, пересохраняет сессию, даже если она не поменялась
+  saveUninitialized: false, // * Если false, куки появляются только при установке req.session
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 * 10, // * время жизни в ms (10 дней)
+    httpOnly: true, // * куки только по http
+  },
+};
+
+// Создание сессии
+app.use(session(sessionConfig));
+
+// Отрисовка страниц res.render()
+app.use(ssrEngine);
+
+// app.use('/', indexRouter);
+
+console.log('here');
+
+app.listen(PORT, async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Соединение с базой установлено!');
+  } catch (err) {
+    console.log(err, 'Error!');
+  }
+  console.log(`Сервер поднят на ${PORT} порту!`);
+});
